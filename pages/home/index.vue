1a6b5c5262353c369e9cfcedfa43b1b36c7ed261
liqiyuan <970445119@qq.com>
2021-04-20 17:28:29 +0800

    自动化部署测试

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
 	<div class="home-page">
 		<div class="banner">
 			<div class="container">
-				<h1 class="logo-font">conduit</h1>
+				<h1 class="logo-font">拉勾教育nuxt项目和自动部练习</h1>
 				<p>A place to share your knowledge.</p>
 			</div>
 		</div>
